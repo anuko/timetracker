cc042da1e9864256191b4a28671059a08d5efa91
anuko <support@anuko.com>
2017-02-12 15:55:37 +0000

    Added an edit form for predefined expenses.

---
+++ predefined_expense_edit.php
@@ -0,0 +1,76 @@
+<?php
+// +----------------------------------------------------------------------+
+// | Anuko Time Tracker
+// +----------------------------------------------------------------------+
+// | Copyright (c) Anuko International Ltd. (https://www.anuko.com)
+// +----------------------------------------------------------------------+
+// | LIBERAL FREEWARE LICENSE: This source code document may be used
+// | by anyone for any purpose, and freely redistributed alone or in
+// | combination with other software, provided that the license is obeyed.
+// |
+// | There are only two ways to violate the license:
+// |
+// | 1. To redistribute this code in source form, with the copyright
+// |    notice or license removed or altered. (Distributing in compiled
+// |    forms without embedded copyright notices is permitted).
+// |
+// | 2. To redistribute modified versions of this code in *any* form
+// |    that bears insufficient indications that the modifications are
+// |    not the work of the original author(s).
+// |
+// | This license applies to this document only, not any other software
+// | that it may be combined with.
+// |
+// +----------------------------------------------------------------------+
+// | Contributors:
+// | https://www.anuko.com/time_tracker/credits.htm
+// +----------------------------------------------------------------------+
+
+require_once('initialize.php');
+import('form.Form');
+import('ttPredefinedExpenseHelper');
+
+// Access check.
+if (!ttAccessCheck(right_manage_team) || !$user->isPluginEnabled('ex')) {
+  header('Location: access_denied.php');
+  exit();
+}
+
+$predefined_expense_id = (int) $request->getParameter('id');
+
+if ($request->isPost()) {
+  $cl_name = trim($request->getParameter('name'));
+  $cl_cost = trim($request->getParameter('cost'));
+} else {
+  $predefined_expense = ttPredefinedExpenseHelper::get($predefined_expense_id);
+  $cl_name = $predefined_expense['name'];
+  $cl_cost = $predefined_expense['cost'];
+}
+
+$form = new Form('predefinedExpenseForm');
+$form->addInput(array('type'=>'hidden','name'=>'id','value'=>$predefined_expense_id));
+$form->addInput(array('type'=>'text','maxlength'=>'100','name'=>'name','style'=>'width: 250px;','value'=>$cl_name));
+$form->addInput(array('type'=>'text','maxlength'=>'40','name'=>'cost','style'=>'width: 100px;','value'=>$cl_cost));
+$form->addInput(array('type'=>'submit','name'=>'btn_submit','value'=>$i18n->getKey('button.submit')));
+
+if ($request->isPost()) {
+  // Validate user input.
+  if (!ttValidString($cl_name)) $err->add($i18n->getKey('error.field'), $i18n->getKey('label.thing_name'));
+  if (!ttValidFloat($cl_cost)) $err->add($i18n->getKey('error.field'), $i18n->getKey('label.cost'));
+  if ($err->no()) {
+    if (ttPredefinedExpenseHelper::update(array(
+        'id' => $predefined_expense_id,
+        'team_id' => $user->team_id,
+        'name' => $cl_name,
+        'cost' => $cl_cost))) {
+        header('Location: predefined_expenses.php');
+        exit();
+      } else
+        $err->add($i18n->getKey('error.db'));
+  }
+} // isPost
+
+$smarty->assign('forms', array($form->getName()=>$form->toArray()));
+$smarty->assign('title', $i18n->getKey('title.edit_predefined_expense'));
+$smarty->assign('content_page_name', 'predefined_expense_edit.tpl');
+$smarty->display('index.tpl');
