5af2ebe2ffdf24289dcb5adf0f87b310707eff71
Jens Brinch <jbmoller@users.noreply.github.com>
2017-02-19 17:37:58 +0100

    Update of translation in da.lang.php (#30)
    Have filled in some missing translations and updated some with a new translation, i.e. correction of spelling errors.

--- WEB-INF/resources/da.lang.php
+++ WEB-INF/resources/da.lang.php
@@ -44,23 +44,23 @@ $i18n_key_words = array(
 'menu.login' => 'Login',
 'menu.logout' => 'Logout',
 // TODO: translate the following:
-// 'menu.forum' => 'Forum',
+'menu.forum' => 'Forum',
 'menu.help' => 'Hjælp',
 // Note to translators: menu.create_team needs a more accurate translation.
-'menu.create_team' => 'lav en ny manager konto',
+'menu.create_team' => 'Lav en ny manager konto',
 'menu.profile' => 'Profil',
 'menu.time' => 'Tid',
 // TODO: translate the following:
-// 'menu.expenses' => 'Expenses'
+'menu.expenses' => 'Udgifter',
 'menu.reports' => 'Rapporter',
 // TODO: translate the following:
 // 'menu.charts' => 'Charts',
 'menu.projects' => 'Projekter',
 // TODO: translate the following:
-// 'menu.tasks' => 'Tasks',
-// 'menu.users' => 'Users',
-// 'menu.teams' => 'Teams',
-// 'menu.export' => 'Export',
+'menu.tasks' => 'Tasks',
+'menu.users' => 'Brugere',
+'menu.teams' => 'Team',
+'menu.export' => 'Eksport',
 'menu.clients' => 'Kunder',
 'menu.options' => 'Indstillinger',
 
@@ -68,31 +68,32 @@ $i18n_key_words = array(
 // TODO: translate the following:
 // 'footer.contribute_msg' => 'You can contribute to Time Tracker in different ways.',
 // 'footer.credits' => 'Credits',
-// 'footer.license' => 'License',
-// 'footer.improve' => 'Contribute', // Translators: this could mean "Improve", if it makes better sense in your language.
+'footer.license' => 'Licens',
+'footer.improve' => 'Bidrag', // Translators: this could mean "Improve", if it makes better sense in your language.
                                      // This is a link to a webpage that describes how to contribute to the project.
 
 // Error messages.
 // TODO: Translate the following:
-// 'error.access_denied' => 'Access denied.',
+'error.access_denied' => 'Adgang nægtet',
 // 'error.sys' => 'System error.',
-'error.db' => 'database fejl',
+'error.db' => 'Database fejl',
 'error.field' => 'forkert "{0}" data',
 'error.empty' => 'felt "{0}" er tom',
 'error.not_equal' => 'felt "{0}" er ikke lig med "{1}"',
 'error.interval' => 'forkert interval',
-'error.project' => 'vælg projekt',
-'error.activity' => 'vælg aktivitet',
-'error.auth' => 'forkert login eller password',
+'error.project' => 'Vælg projekt',
+'error.activity' => 'Vælg aktivitet',
+'error.auth' => 'Forkert login eller password',
 // Note to translators: 'error.user_exists' => 'der eksitrerer en bruger med denne e-mail adresse', // e-mail must be changed to login.
-'error.project_exists' => 'der eksiterer allerede et projekt med det navn',
-'error.activity_exists' => 'aktivitet med det navn eksisterer allerede',
+'error.user_exists' => 'Der eksitrerer en bruger med denne e-mail adresse',
+'error.project_exists' => 'Der eksiterer allerede et projekt med det navn',
+'error.activity_exists' => 'Der eksisterer allerede en aktivitet med det navn eksisterer allerede',
 // TODO: translate error.client_exists.
-// 'error.client_exists' => 'client with this name already exists',
+'error.client_exists' => 'Der eksisterer allerede en klient med dette navn',
 'error.no_login' => 'ingen bruger med denne login',
 'error.upload' => 'fil upload problem',
 // TODO: translate the following:
-// 'error.range_locked' => 'Date range is locked.',
+'error.range_locked' => 'Dato interval er spærret',
 // 'error.mail_send' => 'Error sending mail.',
 // 'error.no_email' => 'No email associated with this login.',
 // 'error.uncompleted_exists' => 'Uncompleted entry already exists. Close or delete it.',
@@ -117,18 +118,18 @@ $i18n_key_words = array(
 // Note to translators: button.reset_password needs an improved translation.
 'button.reset_password' => 'gе til',
 // Note to translators: the strings below must be translated
-// 'button.send' => 'send',
-// 'button.send_by_email' => 'send som e-mail',
-'button.save_as_new' => 'gem som ny',
+'button.send' => 'Send',
+'button.send_by_email' => 'Send som e-mail',
+'button.save_as_new' => 'Gem som ny',
 // TODO: check translation of button.create_team
-// 'button.create_team' => 'lav en team',
-'button.export' => 'exporter team',
-'button.import' => 'importer team',
-'button.apply' => 'gem',
+'button.create_team' => 'Lav et team',
+'button.export' => 'Exporter team',
+'button.import' => 'Importer team',
+'button.apply' => 'Gem',
 
 // labels for controls on various forms
 // TODO: translate label.team_name
-// 'label.team_name' => 'team name',
+'label.team_name' => 'Team navn',
 'label.currency' => 'møntfod',
 // TODO: translate label.manager_name and label.manager_login.
 // 'label.manager_name' => 'manager name',
@@ -137,7 +138,7 @@ $i18n_key_words = array(
 
 'label.password' => 'adgangskode',
 'label.confirm_password' => 'gentag adgangskode',
-'label.email' => 'email',
+'label.email' => 'E-mail',
 
 // Form titles.
 'title.options' => 'Indstillinger',
@@ -148,7 +149,7 @@ $i18n_key_words = array(
 "form.filter.filter" => 'favorit rapport',
 "form.filter.filter_new" => 'gem som favorit',
 // Note to translators: the string below is missing in the translation and must be added
-// "form.filter.filter_confirm_delete" => 'are you sure you want to delete this favorite report?',
+"form.filter.filter_confirm_delete" => 'Er du sikker på at du vil slette denne favorit rapport?',
 
 // login form attributes
 "form.login.title" => 'login',
@@ -158,7 +159,7 @@ $i18n_key_words = array(
 "form.fpass.title" => 'nulstil adgangskode',
 "form.fpass.login" => 'login',
 "form.fpass.send_pass_str" => 'ønske om ny adgangskode sendt',
-"form.fpass.send_pass_subj" => 'Anuko Time Tracker adgangskode nulstil ',
+"form.fpass.send_pass_subj" => 'Nulstil Anuko Time Tracker adgangskode',
 // Note to translators: this string needs to be translated.
 // "form.fpass.send_pass_body" => "Dear User,\n\nSomeone, possibly you, requested your Anuko Time Tracker password reset. Please visit this link if you want to reset your password.\n\n%s\n\nAnuko Time Tracker is a simple, easy to use, open source time tracking system. Visit https://www.anuko.com for more information.\n\n",
 "form.fpass.reset_comment" => "for at nulstille din adgangskode, tast det og klik gem",
@@ -167,31 +168,31 @@ $i18n_key_words = array(
 "form.admin.title" => 'administrator',
 // Note to translators: "form.admin.duty_text" => 'Lav et nyt team, ved at lave en team manager konto.<br>Du kan ogsе importerer fra en xml fil fra en anden Anuko Time Tracker server (no login collisions are allowed).', // the phrase in brackets must be translated
 
-"form.admin.change_pass" => 'skift adgagnskode pе administrator konto',
-"form.admin.profile.title" => 'teams',
-"form.admin.profile.noprofiles" => 'din database er tom, login som administrator og lav et nyt team',
-"form.admin.profile.comment" => 'slet team',
-"form.admin.profile.th.id" => 'id',
+"form.admin.change_pass" => 'Skift adgangskode på administrator konto',
+"form.admin.profile.title" => 'Teams',
+"form.admin.profile.noprofiles" => 'Din database er tom, login som administrator og lav et nyt team',
+"form.admin.profile.comment" => 'Slet team',
+"form.admin.profile.th.id" => 'ID',
 "form.admin.profile.th.name" => 'navn',
 "form.admin.profile.th.edit" => 'rediger',
 "form.admin.profile.th.del" => 'slet',
 "form.admin.profile.th.active" => 'aktive',
-// "form.admin.custom_date_format" => "date format",
-// "form.admin.custom_time_format" => "time format",
-// "form.admin.start_week" => "first day of week",
+"form.admin.custom_date_format" => "Dato format",
+"form.admin.custom_time_format" => "Tids format",
+"form.admin.start_week" => "Første dag i ugen",
 
 // my time form attributes
 "form.mytime.title" => 'min tid',
 "form.mytime.edit_title" => 'rediger tids post',
 "form.mytime.del_str" => 'slet tids post',
 // Note to translators: "form.mytime.time_form" => ' (hh:mm)', // the string must be translated
-"form.mytime.date" => 'dato',
-"form.mytime.project" => 'projekt',
-"form.mytime.activity" => 'aktivitet',
-"form.mytime.start" => 'start',
-"form.mytime.finish" => 'slut',
-"form.mytime.duration" => 'varighed',
-"form.mytime.note" => 'notat',
+"form.mytime.date" => 'Dato',
+"form.mytime.project" => 'Projekt',
+"form.mytime.activity" => 'Aktivitet',
+"form.mytime.start" => 'Start',
+"form.mytime.finish" => 'Slut',
+"form.mytime.duration" => 'Varighed',
+"form.mytime.note" => 'Notat',
 "form.mytime.behalf" => 'dagligt arbejde for',
 "form.mytime.daily" => 'dagligt arbejde',
 "form.mytime.total" => 'timer i alt: ',
@@ -206,32 +207,32 @@ $i18n_key_words = array(
 "form.mytime.del_yes" => 'tids post slettet',
 "form.mytime.no_finished_rec" => 'denne post er gemt med kun en start tid. Det er ikke nødvendigvis en fejl. Du kan nu logge af.',
 // Note to translators: the 3 strings below are missing in the translation and need to be added
-// "form.mytime.billable" => 'billable',
+"form.mytime.billable" => 'Fakturerbar',
 // "form.mytime.warn_tozero_rec" => 'this time record must be deleted because this time period is locked',
 // "form.mytime.uncompleted" => 'uncompleted',
 
 // profile form attributes
 // Note to translators: we need a more accurate translation of form.profile.create_title
 "form.profile.create_title" => 'Dan ny manager konot',
-"form.profile.edit_title" => 'rediger profil',
-"form.profile.name" => 'navn',
-"form.profile.login" => 'login', 
+"form.profile.edit_title" => 'Rediger profil',
+"form.profile.name" => 'Navn',
+"form.profile.login" => 'Login', 
 
 // Note to translators: the strings below are missing in the translation and need to be added
 // "form.profile.showchart" => 'show pie charts',
-// "form.profile.lang" => 'language',
-// "form.profile.custom_date_format" => "date format",
-// "form.profile.custom_time_format" => "time format",
+"form.profile.lang" => 'Sprog',
+"form.profile.custom_date_format" => "Dato format",
+"form.profile.custom_time_format" => "Tids format",
 // "form.profile.default_format" => "(default)",
-// "form.profile.start_week" => "first day of week",
+"form.profile.start_week" => "Første dag i ugen",
 
 // people form attributes
 "form.people.ppl_str" => 'Brugere',
 "form.people.createu_str" => 'Dan ny bruger',
-"form.people.edit_str" => 'reidger bruger',
-"form.people.del_str" => 'slet bruger',
-"form.people.th.name" => 'navn',
-"form.people.th.login" => 'login', 
+"form.people.edit_str" => 'Rediger bruger',
+"form.people.del_str" => 'Slet bruger',
+"form.people.th.name" => 'Navn',
+"form.people.th.login" => 'Login', 
 "form.people.th.role" => 'rolle',
 "form.people.th.edit" => 'rediger',
 "form.people.th.del" => 'slet',
@@ -239,7 +240,7 @@ $i18n_key_words = array(
 "form.people.th.project" => 'projekt',
 "form.people.th.rate" => 'rate',
 "form.people.manager" => 'manager',
-"form.people.comanager" => 'comanager',
+"form.people.comanager" => 'co-manager',
 "form.people.empl" => 'bruger',
 "form.people.name" => 'navn',
 "form.people.login" => 'login', 
@@ -249,26 +250,26 @@ $i18n_key_words = array(
 "form.people.projects" => 'projekter',
 
 // projects form attributes
-"form.project.proj_title" => 'projekter',
-"form.project.edit_str" => 'rediger projekter',
-"form.project.add_str" => 'tilføj projekt', 
-"form.project.del_str" => 'slet projekt',
-"form.project.th.name" => 'navn',
-"form.project.th.edit" => 'rediger',
-"form.project.th.del" => 'slet',
+"form.project.proj_title" => 'Projekter',
+"form.project.edit_str" => 'Rediger projekter',
+"form.project.add_str" => 'Tilføj projekt', 
+"form.project.del_str" => 'Slet projekt',
+"form.project.th.name" => 'Navn',
+"form.project.th.edit" => 'Rediger',
+"form.project.th.del" => 'Slet',
 "form.project.name" => 'navn',
 
 // activities form attributes
-"form.activity.act_title" => 'aktiviteter',
-"form.activity.add_title" => 'tilføj ny aktivitet', 
-"form.activity.edit_str" => 'rediger aktivitet',
-"form.activity.del_str" => 'slet aktivitet',
-"form.activity.name" => 'navn',
-"form.activity.project" => 'projekt',
-"form.activity.th.name" => 'navn',
-"form.activity.th.project" => 'projekt',
-"form.activity.th.edit" => 'rediger',
-"form.activity.th.del" => 'slet',
+"form.activity.act_title" => 'Aktiviteter',
+"form.activity.add_title" => 'Tilføj ny aktivitet', 
+"form.activity.edit_str" => 'Rediger aktivitet',
+"form.activity.del_str" => 'Slet aktivitet',
+"form.activity.name" => 'Navn',
+"form.activity.project" => 'Projekt',
+"form.activity.th.name" => 'Navn',
+"form.activity.th.project" => 'Projekt',
+"form.activity.th.edit" => 'Rediger',
+"form.activity.th.del" => 'Slet',
 
 // report attributes
 "form.report.title" => 'rapport',
@@ -280,7 +281,7 @@ $i18n_key_words = array(
 "form.report.duration" => 'varighed',
 "form.report.start" => 'start',
 "form.report.activity" => 'aktivitet',
-"form.report.show_idle" => 'hvis ledig tid',
+"form.report.show_idle" => 'Ledig tid',
 "form.report.finish" => 'slut',
 "form.report.note" => 'notat',
 "form.report.project" => 'projekt',
@@ -301,42 +302,42 @@ $i18n_key_words = array(
 "form.mail.cc" => 'cc',
 "form.mail.subject" => 'emne',
 "form.mail.comment" => 'komment',
-"form.mail.above" => 'send denne rapport pr. email',
+"form.mail.above" => 'send denne rapport pr. e-mail',
 // Note to translators: this string needs to be translated.
 // "form.mail.footer_str" => 'Anuko Time Tracker is a simple, easy to use, open source<br>time tracking system. Visit <a href="https://www.anuko.com">www.anuko.com</a> for more information.',
-"form.mail.sending_str" => '<b>mail sendt</b>',
+"form.mail.sending_str" => '<b>E-mail sendt</b>',
 
 // invoice attributes
-"form.invoice.title" => 'faktura',
-"form.invoice.caption" => 'faktura',
-"form.invoice.above" => 'Yderligere information for faktura',
-"form.invoice.select_cust" => 'vælg kunde', 
+"form.invoice.title" => 'Faktura',
+"form.invoice.caption" => 'Faktura',
+"form.invoice.above" => 'Yderligere information om faktura',
+"form.invoice.select_cust" => 'Vælg kunde', 
 "form.invoice.fillform" => 'udfyld felterne',
 "form.invoice.date" => 'dato',
-"form.invoice.number" => 'faktura nummer',
-"form.invoice.tax" => 'skat',
-"form.invoice.comment" => 'kommentar',
+"form.invoice.number" => 'Faktura nummer',
+"form.invoice.tax" => 'Moms',
+"form.invoice.comment" => 'Kommentar',
 "form.invoice.th.username" => 'person',
 "form.invoice.th.time" => 'timer',
 "form.invoice.th.rate" => 'rate',
 "form.invoice.th.summ" => 'beløb', 
 "form.invoice.subtotal" => 'subtotal',
 "form.invoice.customer" => 'kunde',
-"form.invoice.mailinv_above" => 'send denne faktura pr. e-mail',
-"form.invoice.sending_str" => '<b>faktura sendt</b>',
+"form.invoice.mailinv_above" => 'Send denne faktura pr. e-mail',
+"form.invoice.sending_str" => '<b>Faktura sendt</b>',
 
 "form.migration.zip" => 'komprimering',
-"form.migration.file" => 'vælg fil', 
+"form.migration.file" => 'Vælg fil', 
 "form.migration.import.title" => 'import data',
 "form.migration.import.success" => 'import gennemført', 
 "form.migration.import.text" => 'import team data fra en xml fil',
-"form.migration.export.title" => 'export data',
-"form.migration.export.success" => 'export gennemført', 
-"form.migration.export.text" => 'Du kan eksporerer data til enxml fil. det kan være praktisk hvis du flytter til egen server.', 
+"form.migration.export.title" => 'Eksport data',
+"form.migration.export.success" => 'Eksport gennemført', 
+"form.migration.export.text" => 'Du kan eksporterer data til en xml fil. Dette kan være praktisk, hvis du flytter til egen server.', 
 // Note to translators: the 3 strings below are missing in the translation and must be added
-// "form.migration.compression.none" => 'none',
-// "form.migration.compression.gzip" => 'gzip',
-// "form.migration.compression.bzip" => 'bzip',
+"form.migration.compression.none" => 'Ingen',
+"form.migration.compression.gzip" => 'gzip',
+"form.migration.compression.bzip" => 'bzip',
 
 "form.client.title" => 'kunder',
 "form.client.add_title" => 'tilføj kunde', 
@@ -346,7 +347,7 @@ $i18n_key_words = array(
 "form.client.th.edit" => 'rediger',
 "form.client.th.del" => 'slet',
 "form.client.name" => 'naavn',
-"form.client.tax" => 'skat',
+"form.client.tax" => 'Moms',
 "form.client.comment" => 'kommenter ',
 
 // miscellaneous strings
@@ -355,9 +356,9 @@ $i18n_key_words = array(
 "forward.delete" => 'slet',
 "forward.tocsvfile" => 'exporter data til .csv fil',
 // Note to translators:  the string below is missing in the translation and must be added
-// "forward.toxmlfile" => 'export data to .xml file',
-"forward.geninvoice" => 'dan faktura',
-"forward.change" => 'konfigurer kunder',
+"forward.toxmlfile" => 'Eksport data som xml fil',
+"forward.geninvoice" => 'Dan faktura',
+"forward.change" => 'Konfigurer kunder',
 
 // strings inside contols on forms
 "controls.select.project" => '--- vælg projekt ---',
@@ -371,20 +372,20 @@ $i18n_key_words = array(
 "controls.per_tw" => 'denne uge',
 "controls.per_lw" => 'sidste uge',
 // Note to translators: the 3 strings below are missing in the translation and must be added
-// "controls.per_td" => 'this day',
-// "controls.per_at" => 'all time',
-// "controls.per_ty" => 'this year',
+"controls.per_td" => 'I dag',
+"controls.per_at" => 'Total tid',
+"controls.per_ty" => 'I år',
 "controls.sel_period" => '--- vælg tids periode ---',
 "controls.sel_groupby" => '--- vælg gruppe ---', 
 // Note to translators: the 3 strings below are missing in the translation and must be added
-// "controls.inc_billable" => 'billable',
-// "controls.inc_nbillable" => 'not billable',
+"controls.inc_billable" => 'Fakturerbar',
+"controls.inc_nbillable" => 'Ikke fakturerbar',
 // "controls.default" => '--- default ---',
 
 // labels
 // Note to translators: the 3 strings below are missing in the translation and must be added
-//"label.chart.title1" => 'activities for user',
-// "label.chart.title2" => 'projects for user',
+"label.chart.title1" => 'Bruger aktiviteter',
+"label.chart.title2" => 'Bruger projekter',
 // "label.chart.period" => 'chart for period',
 
 "label.pinfo" => '%s, %s',
@@ -394,7 +395,7 @@ $i18n_key_words = array(
 "label.pcminfo" => ' (co-manager)',
 "label.painfo" => ' (administrator)',
 "label.time_noentry" => 'ingen input',
-"label.today" => 'idag',
+"label.today" => 'I dag',
 "label.req_fields"=> '* krævede felter', 
 "label.sel_project" => 'vælg projekt',
 "label.sel_activity" => 'vælg aktivtet',
@@ -404,7 +405,7 @@ $i18n_key_words = array(
 "label.group_title" => 'gruper',
 // Note to translators: the string below is missing in the translation and must be added
 // "label.include_title" => 'include records',
-"label.inv_str" => 'faktura',
+"label.inv_str" => 'Faktura',
 "label.set_empl" => 'vælg brugere',
 "label.sel_all" => 'vælg alle',
 "label.sel_none" => 'fravælg alle', 
@@ -419,5 +420,5 @@ $i18n_key_words = array(
 // "label.ldap_hint" => 'Type your <b>Windows login</b> and <b>password</b> in the fields below.',
 
 // login hello text
-// "login.hello.text" => "Anuko Time Tracker is a simple, easy to use, open source time tracking system.",
+"login.hello.text" => "Anuko Time Tracker er et let anvendeligt Open Source værktøj til tidsregistrering.",
 );
