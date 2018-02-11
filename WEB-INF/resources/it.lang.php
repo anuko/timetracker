da93a87e5052017f10005e12c39f7d1023e013fe
Nik Okuntseff <support@anuko.com>
2018-02-11 20:59:12 +0000

    Cosmetic.

--- WEB-INF/resources/it.lang.php
+++ WEB-INF/resources/it.lang.php
@@ -422,6 +422,8 @@ $i18n_key_words = array(
 // Profile form. See example at https://timetracker.anuko.com/profile_edit.php.
 'form.profile.12_hours' => '12 ore',
 'form.profile.24_hours' => '24 ore',
+// // TODO: translate the following.
+// 'form.profile.show_holidays' => 'Show holidays',
 'form.profile.tracking_mode' => 'Modalità di registrazione',
 'form.profile.mode_time' => 'tempo',
 'form.profile.mode_projects' => 'progetti',
