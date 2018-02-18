20f744b683cc7fd47c222d423ae56fa02aad64a2
Nik Okuntseff <support@anuko.com>
2018-02-18 16:08:49 +0000

    Cosmetic - changed the order of input data checking to match control layout.

--- user_edit.php
+++ user_edit.php
@@ -173,9 +173,9 @@ if ($request->isPost()) {
       $err->add($i18n->getKey('error.not_equal'), $i18n->getKey('label.password'), $i18n->getKey('label.confirm_password'));
   }
   if (!ttValidEmail($cl_email, true)) $err->add($i18n->getKey('error.field'), $i18n->getKey('label.email'));
-  if (!ttValidFloat($cl_rate, true)) $err->add($i18n->getKey('error.field'), $i18n->getKey('form.users.default_rate'));
   // Require selection of a client for a client role.
   if ($user->isPluginEnabled('cl') && $cl_role == ROLE_CLIENT && !$cl_client_id) $err->add($i18n->getKey('error.client'));
+  if (!ttValidFloat($cl_rate, true)) $err->add($i18n->getKey('error.field'), $i18n->getKey('form.users.default_rate'));
 
   if ($err->no()) {
     $existing_user = ttUserHelper::getUserByLogin($cl_login);
