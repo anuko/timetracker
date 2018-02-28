3eff870011f63ebcd0cf13c3aa3696e2502ba791
Nik Okuntseff <support@anuko.com>
2018-02-28 17:32:38 +0000

    Added current db version to dbinstall.php output.

--- dbinstall.php
+++ dbinstall.php
@@ -155,6 +155,16 @@ if ($request->isGet()) {
   } else {
     echo('<font color="red">There are no tables in database. Execute step 1 - Create database structure.</font><br>');
   }
+
+  $sql = "select param_value from tt_site_config where param_name = 'version_db'";
+  $res = $conn->query($sql);
+  if (is_a($res, 'MDB2_Error')) {
+    die('<font color="red">Error: database schema version query failed. '.$res->getMessage().'</font><br>');
+  } else {
+    $val = $res->fetchRow();
+    echo('Current database version is: '.$val['param_value'].'.');
+  }
+
   $conn->disconnect();
 }
 
@@ -712,7 +722,7 @@ if ($_POST) {
     setChange("ALTER TABLE `tt_log` ADD `paid` tinyint(4) NULL default '0' AFTER `billable`");
   }
 
-  if ($_POST["convert11400to11734"]) {
+  if ($_POST["convert11400to11735"]) {
     setChange("ALTER TABLE `tt_teams` DROP `address`");
     setChange("ALTER TABLE `tt_fav_reports` ADD `report_spec` text default NULL AFTER `user_id`");
     setChange("ALTER TABLE `tt_fav_reports` ADD `paid_status` tinyint(4) default NULL AFTER `invoice`");
@@ -787,7 +797,7 @@ if ($_POST) {
 <h2>DB Install</h2>
 <table width="80%" border="1" cellpadding="10" cellspacing="0">
   <tr>
-    <td width="80%"><b>Create database structure (v1.17.34)</b>
+    <td width="80%"><b>Create database structure (v1.17.35)</b>
     <br>(applies only to new installations, do not execute when updating)</br></td><td><input type="submit" name="crstructure" value="Create"></td>
   </tr>
 </table>
@@ -823,8 +833,8 @@ if ($_POST) {
     <td><input type="submit" name="convert1600to11400" value="Update"><br></td>
   </tr>
   <tr valign="top">
-    <td>Update database structure (v1.14 to v1.17.34)</td>
-    <td><input type="submit" name="convert11400to11734" value="Update"><br></td>
+    <td>Update database structure (v1.14 to v1.17.35)</td>
+    <td><input type="submit" name="convert11400to11735" value="Update"><br></td>
   </tr>
 </table>
 
