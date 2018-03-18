128f452ba21507479cace52c4b0e1ca11fffd2ed
Nik Okuntseff <support@anuko.com>
2018-03-18 17:07:03 +0000

    Updated version.

--- dbinstall.php
+++ dbinstall.php
@@ -805,7 +805,7 @@ if ($_POST) {
     print "Updated $users_updated users...<br>\n";
   }
 
-  if ($_POST["convert11744to11752"]) {
+  if ($_POST["convert11744to11753"]) {
     setChange("update `tt_roles` inner join `tt_site_config` sc on (sc.param_name = 'version_db' and sc.param_value = '1.17.44') set rights = replace(rights, 'override_punch_mode,override_date_lock', 'override_punch_mode,override_own_punch_mode,override_date_lock')");
     setChange("UPDATE `tt_site_config` SET param_value = '1.17.48' where param_name = 'version_db' and param_value = '1.17.44'");
     setChange("update `tt_users` inner join `tt_site_config` sc on (sc.param_name = 'version_db' and sc.param_value = '1.17.48') set role_id = (select id from tt_roles where team_id = 0 and rank = 512) where role = 324");
@@ -870,7 +870,7 @@ if ($_POST) {
 <h2>DB Install</h2>
 <table width="80%" border="1" cellpadding="10" cellspacing="0">
   <tr>
-    <td width="80%"><b>Create database structure (v1.17.52)</b>
+    <td width="80%"><b>Create database structure (v1.17.53)</b>
     <br>(applies only to new installations, do not execute when updating)</br></td><td><input type="submit" name="crstructure" value="Create"></td>
   </tr>
 </table>
@@ -910,8 +910,8 @@ if ($_POST) {
     <td><input type="submit" name="convert11400to11744" value="Update"><br><input type="submit" name="update_role_id" value="Update role_id"></td>
   </tr>
     <tr valign="top">
-    <td>Update database structure (v1.17.44 to v1.17.52)</td>
-    <td><input type="submit" name="convert11744to11752" value="Update"></td>
+    <td>Update database structure (v1.17.44 to v1.17.53)</td>
+    <td><input type="submit" name="convert11744to11753" value="Update"></td>
   </tr>
 </table>
 
