05cc6905eba759b924e2dc4efc884a930711f928
Nik Okuntseff <support@anuko.com>
2018-11-25 14:17:17 +0000

    Added group selector on projects.php.

--- projects.php
+++ projects.php
@@ -39,14 +39,41 @@ if (MODE_PROJECTS != $user->tracking_mode && MODE_PROJECTS_AND_TASKS != $user->t
   header('Location: feature_disabled.php');
   exit();
 }
+if ($request->isPost() && !$user->isGroupValid($request->getParameter('group'))) {
+  header('Location: access_denied.php'); // Wrong group id in post.
+  exit();
+}
 // End of access checks.
 
+if ($request->isPost()) {
+  $group_id = $request->getParameter('group');
+  $user->setOnBehalfGroup($group_id);
+} else {
+  $group_id = $user->getActiveGroup();
+}
+
+$form = new Form('projectsForm');
+if ($user->can('manage_subgroups')) {
+  $groups = $user->getGroupsForDropdown();
+  if (count($groups) > 1) {
+    $form->addInput(array('type'=>'combobox',
+      'onchange'=>'this.form.submit();',
+      'name'=>'group',
+      'style'=>'width: 250px;',
+      'value'=>$group_id,
+      'data'=>$groups,
+      'datakeys'=>array('id','name')));
+    $smarty->assign('group_dropdown', 1);
+  }
+}
+
 if($user->can('manage_projects')) {
-  $active_projects = ttTeamHelper::getActiveProjects($user->group_id);
-  $inactive_projects = ttTeamHelper::getInactiveProjects($user->group_id);
+  $active_projects = ttTeamHelper::getActiveProjects($group_id);
+  $inactive_projects = ttTeamHelper::getInactiveProjects($group_id);
 } else
   $active_projects = $user->getAssignedProjects();
 
+$smarty->assign('forms', array($form->getName()=>$form->toArray()));
 $smarty->assign('active_projects', $active_projects);
 $smarty->assign('inactive_projects', $inactive_projects);
 $smarty->assign('title', $i18n->get('title.projects'));
