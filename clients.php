5be0f37534b82691a7707cde3aeb3d0b41fd71e5
Nik Okuntseff <support@anuko.com>
2018-12-01 17:32:37 +0000

    Resuming refactoring for subgroup support.

--- clients.php
+++ clients.php
@@ -39,13 +39,45 @@ if (!$user->isPluginEnabled('cl')) {
   header('Location: feature_disabled.php');
   exit();
 }
+if ($request->isPost()) {
+  $groupChanged = $request->getParameter('group_changed'); // Reused in multiple places below.
+  if ($groupChanged && !($user->can('manage_subgroups') && $user->isGroupValid($request->getParameter('group')))) {
+    header('Location: access_denied.php'); // Group changed, but no rght or wrong group id.
+    exit();
+  }
+}
+
+// Determine group for which we display this page.
+if ($request->isPost() && $groupChanged) {
+  $group_id = $request->getParameter('group');
+  $user->setOnBehalfGroup($group_id);
+} else {
+  $group_id = $user->getGroup();
+}
+
+$form = new Form('clientsForm');
+if ($user->can('manage_subgroups')) {
+  $groups = $user->getGroupsForDropdown();
+  if (count($groups) > 1) {
+    $form->addInput(array('type'=>'combobox',
+      'onchange'=>'document.clientsForm.group_changed.value=1;document.clientsForm.submit();',
+      'name'=>'group',
+      'style'=>'width: 250px;',
+      'value'=>$group_id,
+      'data'=>$groups,
+      'datakeys'=>array('id','name')));
+    $form->addInput(array('type'=>'hidden','name'=>'group_changed'));
+    $smarty->assign('group_dropdown', 1);
+  }
+}
 
 if($user->can('manage_clients')) {
-  $active_clients = ttTeamHelper::getActiveClients($user->group_id, true);
-  $inactive_clients = ttTeamHelper::getInactiveClients($user->group_id, true);
+  $active_clients = ttTeamHelper::getActiveClients($user->getGroup(), true);
+  $inactive_clients = ttTeamHelper::getInactiveClients($user->getGroup(), true);
 } else
   $active_clients = $user->getAssignedClients();
 
+$smarty->assign('forms', array($form->getName()=>$form->toArray()));
 $smarty->assign('active_clients', $active_clients);
 $smarty->assign('inactive_clients', $inactive_clients);
 $smarty->assign('title', $i18n->get('title.clients'));
