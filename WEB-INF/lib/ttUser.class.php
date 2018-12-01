884b152e088f23cfec4342db48fb9924cc647cee
Nik Okuntseff <support@anuko.com>
2018-12-01 21:30:52 +0000

    Improved clients.php to support subgroups.

--- WEB-INF/lib/ttUser.class.php
+++ WEB-INF/lib/ttUser.class.php
@@ -230,11 +230,14 @@ class ttUser {
     $result = array();
     $mdb2 = getConnection();
 
-    $group_id = $this->behalf_group_id ? $this->behalf_group_id : $this->group_id;
+    $group_id = $this->getGroup();
+    $org_id = $this->org_id;
+    $user_id = $this->getUser();
+
     // Do a query with inner join to get assigned projects.
-    $sql = "select p.id, p.name, p.description, p.tasks, upb.rate from tt_projects p
-      inner join tt_user_project_binds upb on (upb.user_id = ".$this->getUser()." and upb.project_id = p.id and upb.status = 1)
-      where p.group_id = $group_id and p.status = 1 order by p.name";
+    $sql = "select p.id, p.name, p.description, p.tasks, upb.rate from tt_projects p".
+      " inner join tt_user_project_binds upb on (upb.user_id = $user_id and upb.project_id = p.id and upb.status = 1)".
+      " where p.group_id = $group_id and p.org_id = $org_id and p.status = 1 order by p.name";
     $res = $mdb2->query($sql);
     if (!is_a($res, 'PEAR_Error')) {
       while ($val = $res->fetchRow()) {
@@ -288,9 +291,12 @@ class ttUser {
 
     $mdb2 = getConnection();
 
+    $group_id = $this->getGroup();
+    $org_id = $this->org_id;
+
     // Get active clients for group.
     $clients = array();
-    $sql = "select id, name, address, projects from tt_clients where group_id = $this->group_id and status = 1";
+    $sql = "select id, name, address, projects from tt_clients where group_id = $group_id and org_id = $org_id and status = 1";
     $res = $mdb2->query($sql);
     if (!is_a($res, 'PEAR_Error')) {
       while ($val = $res->fetchRow()) {
