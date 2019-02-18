5d4953bb5ec09e6e79a114c71f53f410c6064f2e
Nik Okuntseff <support@anuko.com>
2019-02-18 17:04:48 +0000

    Work in progress on timesheet edit.

---
+++ WEB-INF/templates/timesheet_edit.tpl
@@ -0,0 +1,33 @@
+{$forms.timesheetForm.open}
+<table cellspacing="4" cellpadding="7" border="0">
+  <tr>
+    <td>
+      <table cellspacing="1" cellpadding="2" border="0">
+        <tr>
+          <td align = "right">{$i18n.label.thing_name} (*):</td>
+          <td>{$forms.timesheetForm.timesheet_name.control}</td>
+        </tr>
+        <tr>
+          <td align = "right">{$i18n.label.comment}:</td>
+          <td>{$forms.timesheetForm.comment.control}</td>
+        </tr>
+        <tr>
+          <td align="right">{$i18n.label.status}:</td>
+          <td>{$forms.timesheetForm.status.control}</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>{$i18n.label.required_fields}</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="2" align="center" height="50">{$forms.timesheetForm.btn_save.control}</td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
+{$forms.timesheetForm.close}
--- timesheet_edit.php
+++ timesheet_edit.php
@@ -28,69 +28,51 @@
 
 require_once('initialize.php');
 import('form.Form');
-import('ttProjectHelper');
-import('ttGroupHelper');
+import('ttTimesheetHelper');
 
 // Access checks.
-if (!ttAccessAllowed('manage_projects')) {
+if (!(ttAccessAllowed('manage_own_timesheets') || ttAccessAllowed('manage_timesheets'))) {
   header('Location: access_denied.php');
   exit();
 }
-if (MODE_PROJECTS != $user->getTrackingMode() && MODE_PROJECTS_AND_TASKS != $user->getTrackingMode()) {
+if (!$user->isPluginEnabled('ts')) {
   header('Location: feature_disabled.php');
   exit();
 }
-$cl_project_id = (int)$request->getParameter('id');
-$project = ttProjectHelper::get($cl_project_id);
-if (!$project) {
+$cl_timesheet_id = (int)$request->getParameter('id');
+$timesheet = ttTimesheetHelper::getTimesheet($cl_timesheet_id);
+if (!$timesheet) {
   header('Location: access_denied.php');
   exit();
 }
 // End of access checks.
 
-$users = ttGroupHelper::getActiveUsers();
-foreach ($users as $user_item)
-  $all_users[$user_item['id']] = $user_item['name'];
-
-$tasks = ttGroupHelper::getActiveTasks();
-foreach ($tasks as $task_item)
-  $all_tasks[$task_item['id']] = $task_item['name'];
-$show_tasks = MODE_PROJECTS_AND_TASKS == $user->getTrackingMode() && count($tasks) > 0;
-
 if ($request->isPost()) {
-  $cl_name = trim($request->getParameter('project_name'));
-  $cl_description = trim($request->getParameter('description'));
+  $cl_name = trim($request->getParameter('timesheet_name'));
+  $cl_comment = trim($request->getParameter('comment'));
   $cl_status = $request->getParameter('status');
-  $cl_users = $request->getParameter('users', array());
-  $cl_tasks = $request->getParameter('tasks', array());
 } else {
-  $cl_name = $project['name'];
-  $cl_description = $project['description'];
-  $cl_status = $project['status'];
-  $cl_users = ttProjectHelper::getAssignedUsers($cl_project_id);
-  $cl_tasks = explode(',', $project['tasks']);
+  $cl_name = $timesheet['name'];
+  $cl_comment = $timesheet['submitter_comment'];
+  $cl_status = $timesheet['status'];
 }
 
-$form = new Form('projectForm');
-$form->addInput(array('type'=>'hidden','name'=>'id','value'=>$cl_project_id));
-$form->addInput(array('type'=>'text','maxlength'=>'100','name'=>'project_name','style'=>'width: 250px;','value'=>$cl_name));
-$form->addInput(array('type'=>'textarea','name'=>'description','style'=>'width: 250px; height: 40px;','value'=>$cl_description));
+$form = new Form('timesheetForm');
+$form->addInput(array('type'=>'hidden','name'=>'id','value'=>$cl_timesheet_id));
+$form->addInput(array('type'=>'text','maxlength'=>'100','name'=>'timesheet_name','style'=>'width: 250px;','value'=>$cl_name));
+$form->addInput(array('type'=>'textarea','name'=>'comment','style'=>'width: 250px; height: 40px;','value'=>$cl_comment));
 $form->addInput(array('type'=>'combobox','name'=>'status','value'=>$cl_status,
   'data'=>array(ACTIVE=>$i18n->get('dropdown.status_active'),INACTIVE=>$i18n->get('dropdown.status_inactive'))));
-$form->addInput(array('type'=>'checkboxgroup','name'=>'users','data'=>$all_users,'layout'=>'H','value'=>$cl_users));
-if ($show_tasks)
-  $form->addInput(array('type'=>'checkboxgroup','name'=>'tasks','data'=>$all_tasks,'layout'=>'H','value'=>$cl_tasks));
 $form->addInput(array('type'=>'submit','name'=>'btn_save','value'=>$i18n->get('button.save')));
-$form->addInput(array('type'=>'submit','name'=>'btn_copy','value'=>$i18n->get('button.copy')));
 
 if ($request->isPost()) {
   // Validate user input.
   if (!ttValidString($cl_name)) $err->add($i18n->get('error.field'), $i18n->get('label.thing_name'));
-  if (!ttValidString($cl_description, true)) $err->add($i18n->get('error.field'), $i18n->get('label.description'));
-  if (!ttGroupHelper::validateCheckboxGroupInput($cl_users, 'tt_users')) $err->add($i18n->get('error.field'), $i18n->get('label.users'));
-  if (!ttGroupHelper::validateCheckboxGroupInput($cl_tasks, 'tt_tasks')) $err->add($i18n->get('error.field'), $i18n->get('label.tasks'));
+  if (!ttValidString($cl_comment, true)) $err->add($i18n->get('error.field'), $i18n->get('label.comment'));
 
   if ($err->no()) {
+    /*
+    // TODO: coding ongoing down from here.
     if ($request->getParameter('btn_save')) {
       $existing_project = ttProjectHelper::getProjectByName($cl_name);
       if (!$existing_project || ($cl_project_id == $existing_project['id'])) {
@@ -108,29 +90,14 @@ if ($request->isPost()) {
            $err->add($i18n->get('error.db'));
       } else
         $err->add($i18n->get('error.object_exists'));
-    }
-
-    if ($request->getParameter('btn_copy')) {
-      if (!ttProjectHelper::getProjectByName($cl_name)) {
-        if (ttProjectHelper::insert(array('name' => $cl_name,
-          'description' => $cl_description,
-          'users' => $cl_users,
-          'tasks' => $cl_tasks,
-          'status' => ACTIVE))) {
-          header('Location: projects.php');
-          exit();
-        } else
-          $err->add($i18n->get('error.db'));
-      } else
-        $err->add($i18n->get('error.object_exists'));
-    }
+    }*/
   }
 } // isPost
 
 $smarty->assign('forms', array($form->getName()=>$form->toArray()));
-$smarty->assign('onload', 'onLoad="document.projectForm.project_name.focus()"');
+$smarty->assign('onload', 'onLoad="document.timesheetForm.timesheet_name.focus()"');
 $smarty->assign('show_users', count($users) > 0);
 $smarty->assign('show_tasks', $show_tasks);
-$smarty->assign('title', $i18n->get('title.edit_project'));
-$smarty->assign('content_page_name', 'project_edit.tpl');
+$smarty->assign('title', $i18n->get('title.edit_timesheet'));
+$smarty->assign('content_page_name', 'timesheet_edit.tpl');
 $smarty->display('index.tpl');
