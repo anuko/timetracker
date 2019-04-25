2b39252b38f5bd1be462224fe781a40f5b0ddf95
Nik Okuntseff <support@anuko.com>
2019-04-25 01:34:58 +0000

    Version fix in mysql.sql.

--- mysql.sql
+++ mysql.sql
@@ -609,4 +609,4 @@ CREATE TABLE `tt_site_config` (
   PRIMARY KEY  (`param_name`)
 );
 
-INSERT INTO `tt_site_config` (`param_name`, `param_value`, `created`) VALUES ('version_db', '1.19.2', now()); # TODO: change when structure changes.
+INSERT INTO `tt_site_config` (`param_name`, `param_value`, `created`) VALUES ('version_db', '1.19.3', now()); # TODO: change when structure changes.
