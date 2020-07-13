275fa15951ebfe27ba05572471de9e0ef914680c
Nik Okuntseff <support@anuko.com>
2020-07-13 13:28:47 +0000

    Cosmetic. Removed some white space.

--- WEB-INF/lib/form/Calendar.class.php
+++ WEB-INF/lib/form/Calendar.class.php
@@ -80,7 +80,7 @@ class Calendar extends FormElement {
     $firstOfPreviousMonth = strftime(DB_DATEFORMAT, $firstOfPreviousMonth2AM);
 
     // Print calendar header.
-    $html .= "\n\n\n\n\n\n<!-- start of calendar -->\n";
+    $html .= "\n\n<!-- start of calendar -->\n";
     $html .= '<table cellpadding="0" cellspacing="0" border="0" width="100%">'."\n";
     $html .= '  <tr><td align="center">';
     $html .= '<div class="calendarHeader">';
@@ -201,7 +201,7 @@ class Calendar extends FormElement {
     $html .= "adjustToday();\n";
     $html .= "</script>\n";
 
-    $html .= "<!-- end of calendar -->\n\n\n\n\n\n\n";
+    $html .= "<!-- end of calendar -->\n\n";
     return $html;
   }
 
