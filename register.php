6356d37222081b4689cb6987a3cc8c8bf3ac8c79
Nik Okuntseff <support@anuko.com>
2020-09-28 21:18:08 +0000

    Some more cleanup.

--- register.php
+++ register.php
@@ -118,12 +118,5 @@ if ($request->isPost()) {
 $smarty->assign('title', $i18n->get('title.add_group'));
 $smarty->assign('forms', array($form->getName()=>$form->toArray()));
 $smarty->assign('onload', 'onLoad="document.groupForm.group_name.focus()"');
-if (defined('DEBUG_MOBILE'))
-  $smarty->assign('content_page_name', 'register2.tpl');
-else {
-  $smarty->assign('content_page_name', 'register.tpl');
-}
-if (defined('DEBUG_MOBILE'))
-  $smarty->display('index2.tpl');
-else
-  $smarty->display('index.tpl');
+$smarty->assign('content_page_name', 'register2.tpl');
+$smarty->display('index2.tpl');
