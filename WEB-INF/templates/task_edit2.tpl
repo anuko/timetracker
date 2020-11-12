8824bd2332368f1d463d72f95e6b085cdea49138
Nik Okuntseff <support@anuko.com>
2020-11-12 19:45:49 +0000

    Made task_edit.php mobile-friendly.

---
+++ WEB-INF/templates/task_edit2.tpl
@@ -0,0 +1,33 @@
+{$forms.taskForm.open}
+<table class="centered-table">
+  <tr class = "small-screen-label"><td><label for="name">{$i18n.label.thing_name} (*):</label></td></tr>
+  <tr>
+    <td class="large-screen-label"><label for="name">{$i18n.label.thing_name} (*):</label></td>
+    <td class="td-with-input">{$forms.taskForm.name.control}</td>
+  </tr>
+  <tr><td><div class="small-screen-form-control-separator"></div></td></tr>
+  <tr class = "small-screen-label"><td><label for="description">{$i18n.label.description}:</label></td></tr>
+  <tr>
+    <td class="large-screen-label"><label for="description">{$i18n.label.description}:</label></td>
+    <td class="td-with-input">{$forms.taskForm.description.control}</td>
+  </tr>
+  <tr><td><div class="form-control-separator"></div></td></tr>
+  <tr class = "small-screen-label"><td><label for="status">{$i18n.label.status}:</label></td></tr>
+  <tr>
+    <td class="large-screen-label"><label for="status">{$i18n.label.status}:</label></td>
+    <td class="td-with-input">{$forms.taskForm.status.control}</td>
+  </tr>
+  <tr><td><div class="form-control-separator"></div></td></tr>
+{if $show_projects}
+  <tr class = "small-screen-label"><td>{$i18n.label.projects}:</td></tr>
+  <tr>
+    <td class="large-screen-label">{$i18n.label.projects}:</td>
+    <td class="td-with-input">{$forms.taskForm.projects.control}</td>
+  </tr>
+  <tr><td><div class="form-control-separator"></div></td></tr>
+{/if}
+  <tr><td colspan="2">{$i18n.label.required_fields}</td></tr>
+  <tr><td><div class="form-control-separator"></div></td></tr>
+</table>
+<div class="button-set">{$forms.taskForm.btn_save.control} {$forms.taskForm.btn_copy.control}</div>
+{$forms.taskForm.close}
--- task_edit1.php
+++ task_edit1.php
@@ -1,6 +1,30 @@
 <?php
-/* Copyright (c) Anuko International Ltd. https://www.anuko.com
-License: See license.txt */
+// +----------------------------------------------------------------------+
+// | Anuko Time Tracker
+// +----------------------------------------------------------------------+
+// | Copyright (c) Anuko International Ltd. (https://www.anuko.com)
+// +----------------------------------------------------------------------+
+// | LIBERAL FREEWARE LICENSE: This source code document may be used
+// | by anyone for any purpose, and freely redistributed alone or in
+// | combination with other software, provided that the license is obeyed.
+// |
+// | There are only two ways to violate the license:
+// |
+// | 1. To redistribute this code in source form, with the copyright
+// |    notice or license removed or altered. (Distributing in compiled
+// |    forms without embedded copyright notices is permitted).
+// |
+// | 2. To redistribute modified versions of this code in *any* form
+// |    that bears insufficient indications that the modifications are
+// |    not the work of the original author(s).
+// |
+// | This license applies to this document only, not any other software
+// | that it may be combined with.
+// |
+// +----------------------------------------------------------------------+
+// | Contributors:
+// | https://www.anuko.com/time_tracker/credits.htm
+// +----------------------------------------------------------------------+
 
 require_once('initialize.php');
 import('form.Form');
@@ -42,8 +66,8 @@ if ($request->isPost()) {
 
 $form = new Form('taskForm');
 $form->addInput(array('type'=>'hidden','name'=>'id','value'=>$cl_task_id));
-$form->addInput(array('type'=>'text','maxlength'=>'100','name'=>'name','value'=>$cl_name));
-$form->addInput(array('type'=>'textarea','name'=>'description','value'=>$cl_description));
+$form->addInput(array('type'=>'text','maxlength'=>'100','name'=>'name','style'=>'width: 250px;','value'=>$cl_name));
+$form->addInput(array('type'=>'textarea','name'=>'description','style'=>'width: 250px; height: 40px;','value'=>$cl_description));
 $form->addInput(array('type'=>'combobox','name'=>'status','value'=>$cl_status,
   'data'=>array(ACTIVE=>$i18n->get('dropdown.status_active'),INACTIVE=>$i18n->get('dropdown.status_inactive'))));
 $form->addInput(array('type'=>'checkboxgroup','name'=>'projects','layout'=>'H','data'=>$projects,'datakeys'=>array('id','name'),'value'=>$cl_projects));
@@ -94,5 +118,5 @@ if ($request->isPost()) {
 $smarty->assign('forms', array($form->getName()=>$form->toArray()));
 $smarty->assign('show_projects', count($projects) > 0);
 $smarty->assign('title', $i18n->get('title.edit_task'));
-$smarty->assign('content_page_name', 'task_edit2.tpl');
-$smarty->display('index2.tpl');
+$smarty->assign('content_page_name', 'task_edit.tpl');
+$smarty->display('index.tpl');
