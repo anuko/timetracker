32c1ced1ae5cf2df327ad896df0a4d1aa6c98860
Nik Okuntseff <support@anuko.com>
2020-11-24 14:55:33 +0000

    Made swap_roles.php page mobile-friendly.

---
+++ WEB-INF/templates/swap_roles2.tpl
@@ -0,0 +1,11 @@
+<div class="page-hint">{$i18n.form.swap.hint}</div>
+{$forms.swapForm.open}
+<table class="centered-table">
+  <tr class = "small-screen-label"><td><label for="swap_with">{$i18n.form.swap.swap_with}:</label></td></tr>
+  <tr>
+    <td class="large-screen-label"><label for="swap_with">{$i18n.form.swap.swap_with}:</label></td>
+    <td class="td-with-input">{$forms.swapForm.swap_with.control}</td>
+  </tr>
+</table>
+<div class="button-set">{$forms.swapForm.btn_submit.control} {$forms.swapForm.btn_cancel.control}</div>
+{$forms.swapForm.close}
--- swap_roles.php
+++ swap_roles.php
@@ -1,30 +1,6 @@
 <?php
-// +----------------------------------------------------------------------+
-// | Anuko Time Tracker
-// +----------------------------------------------------------------------+
-// | Copyright (c) Anuko International Ltd. (https://www.anuko.com)
-// +----------------------------------------------------------------------+
-// | LIBERAL FREEWARE LICENSE: This source code document may be used
-// | by anyone for any purpose, and freely redistributed alone or in
-// | combination with other software, provided that the license is obeyed.
-// |
-// | There are only two ways to violate the license:
-// |
-// | 1. To redistribute this code in source form, with the copyright
-// |    notice or license removed or altered. (Distributing in compiled
-// |    forms without embedded copyright notices is permitted).
-// |
-// | 2. To redistribute modified versions of this code in *any* form
-// |    that bears insufficient indications that the modifications are
-// |    not the work of the original author(s).
-// |
-// | This license applies to this document only, not any other software
-// | that it may be combined with.
-// |
-// +----------------------------------------------------------------------+
-// | Contributors:
-// | https://www.anuko.com/time_tracker/credits.htm
-// +----------------------------------------------------------------------+
+/* Copyright (c) Anuko International Ltd. https://www.anuko.com
+License: See license.txt */
 
 require_once('initialize.php');
 import('form.Form');
@@ -51,7 +27,7 @@ if ($request->isPost()) {
 // End of access checks.
 
 $form = new Form('swapForm');
-$form->addInput(array('type'=>'combobox','name'=>'swap_with','style'=>'width: 250px;','data'=>$users_for_swap,'datakeys'=>array('id','name')));
+$form->addInput(array('type'=>'combobox','name'=>'swap_with','data'=>$users_for_swap,'datakeys'=>array('id','name')));
 $form->addInput(array('type'=>'submit','name'=>'btn_submit','value'=>$i18n->get('button.submit')));
 $form->addInput(array('type'=>'submit','name'=>'btn_cancel','value'=>$i18n->get('button.cancel')));
 
@@ -73,5 +49,5 @@ if ($request->isPost()) {
 $smarty->assign('forms', array($form->getName()=>$form->toArray()));
 $smarty->assign('onload', 'onLoad="document.swapForm.btn_cancel.focus()"');
 $smarty->assign('title', $i18n->get('title.swap_roles'));
-$smarty->assign('content_page_name', 'swap_roles.tpl');
-$smarty->display('index.tpl');
+$smarty->assign('content_page_name', 'swap_roles2.tpl');
+$smarty->display('index2.tpl');
