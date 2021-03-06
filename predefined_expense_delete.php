5a9248484ea3248963da71b955449ce6f354580b
Nik Okuntseff <support@anuko.com>
2021-03-06 21:27:05 +0000

    Some cosmetic refactoring.

--- predefined_expense_delete.php
+++ predefined_expense_delete.php
@@ -1,30 +1,6 @@
 <?php
-// +----------------------------------------------------------------------+
-// | Anuko Time Tracker
-// +----------------------------------------------------------------------+
-// | Copyright (c) Anuko International Ltd. (https://www.anuko.com)
-// +----------------------------------------------------------------------+
-// | LIBERAL FREEWARE LICENSE: This source code document may be used
-// | by anyone for any purpose, and freely redistributed alone or in
-// | combination with other software, provided that the license is obeyed.
-// |
-// | There are only two ways to violate the license:
-// |
-// | 1. To redistribute this code in source form, with the copyright
-// |    notice or license removed or altered. (Distributing in compiled
-// |    forms without embedded copyright notices is permitted).
-// |
-// | 2. To redistribute modified versions of this code in *any* form
-// |    that bears insufficient indications that the modifications are
-// |    not the work of the original author(s).
-// |
-// | This license applies to this document only, not any other software
-// | that it may be combined with.
-// |
-// +----------------------------------------------------------------------+
-// | Contributors:
-// | https://www.anuko.com/time_tracker/credits.htm
-// +----------------------------------------------------------------------+
+/* Copyright (c) Anuko International Ltd. https://www.anuko.com
+License: See license.txt */
 
 require_once('initialize.php');
 import('form.Form');
@@ -39,8 +15,8 @@ if (!$user->isPluginEnabled('ex')) {
   header('Location: feature_disabled.php');
   exit();
 }
-$cl_predefined_expense_id = (int)$request->getParameter('id');
-$predefined_expense = ttPredefinedExpenseHelper::get($cl_predefined_expense_id);
+$predefined_expense_id = (int)$request->getParameter('id');
+$predefined_expense = ttPredefinedExpenseHelper::get($predefined_expense_id);
 if (!$predefined_expense) {
   header('Location: access_denied.php');
   exit();
@@ -51,13 +27,13 @@ if (!$predefined_expense) {
 $predefined_expense_to_delete = $predefined_expense['name'];
 
 $form = new Form('predefinedExpenseDeleteForm');
-$form->addInput(array('type'=>'hidden','name'=>'id','value'=>$cl_predefined_expense_id));
+$form->addInput(array('type'=>'hidden','name'=>'id','value'=>$predefined_expense_id));
 $form->addInput(array('type'=>'submit','name'=>'btn_delete','value'=>$i18n->get('label.delete')));
 $form->addInput(array('type'=>'submit','name'=>'btn_cancel','value'=>$i18n->get('button.cancel')));
 
 if ($request->isPost()) {
   if ($request->getParameter('btn_delete')) {
-    if (ttPredefinedExpenseHelper::delete($cl_predefined_expense_id)) {
+    if (ttPredefinedExpenseHelper::delete($predefined_expense_id)) {
       header('Location: predefined_expenses.php');
       exit();
     } else
