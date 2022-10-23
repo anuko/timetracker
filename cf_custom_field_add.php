00a75a7ed04fa22de4211891580deee454f04ec2
Nik Okuntseff <support@anuko.com>
2022-10-23 16:32:14 +0000

    Initial implementation of project custom fields.

--- cf_custom_field_add.php
+++ cf_custom_field_add.php
@@ -27,18 +27,11 @@ if ($request->isPost()) {
 
 $form = new Form('fieldForm');
 $form->addInput(array('type'=>'text','maxlength'=>'100','name'=>'name','value'=>''));
-if (defined('DEBUG_PROJECT_CUSTOM_FIELDS')) {
-  $form->addInput(array('type'=>'combobox','name'=>'entity',
-    'data'=>array(CustomFields::ENTITY_TIME=>$i18n->get('entity.time'),
-                  CustomFields::ENTITY_USER=>$i18n->get('entity.user'),
-                  CustomFields::ENTITY_PROJECT=>$i18n->get('entity.project'))
-  ));
-} else {
-  $form->addInput(array('type'=>'combobox','name'=>'entity',
-    'data'=>array(CustomFields::ENTITY_TIME=>$i18n->get('entity.time'),
-                  CustomFields::ENTITY_USER=>$i18n->get('entity.user'))
-  ));
-}
+$form->addInput(array('type'=>'combobox','name'=>'entity',
+  'data'=>array(CustomFields::ENTITY_TIME=>$i18n->get('entity.time'),
+                CustomFields::ENTITY_USER=>$i18n->get('entity.user'),
+                CustomFields::ENTITY_PROJECT=>$i18n->get('entity.project'))
+));
 $form->addInput(array('type'=>'combobox','name'=>'type',
   'data'=>array(CustomFields::TYPE_TEXT=>$i18n->get('label.type_text'),
                 CustomFields::TYPE_DROPDOWN=>$i18n->get('label.type_dropdown'))
